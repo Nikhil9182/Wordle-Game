43818118a80d4874b47276b1d46cb31189584b6a
Nikhil9182 <91827364n@gmail.com>
2025-03-31 19:37:51 +0530

    Icon and Build Files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Nikhil9182/Wordle-Game",
   plugins: [tailwindcss(), react()],
-})
+});
